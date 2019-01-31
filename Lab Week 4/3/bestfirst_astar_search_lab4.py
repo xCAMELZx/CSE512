083d9c38c73d08872a160a39da6b70c411145805
Yousef Jarrar <005845836@coyote.csusb.edu>
2019-01-31 08:02:12 -0800

    finished #3

---
+++ Lab Week 4/3/bestfirst_astar_search_lab4.py
@@ -0,0 +1,232 @@
+# bestfirst_astar_search_lab4.py
+# by Kerstin Voigt, Jan 2018, for lectures and lab on GRAPHSEARCH
+# new in Jan 2019; use a class Node to bundle all relevant
+# state information
+
+# to be used in lab4
+
+import random
+import time
+import pickle
+from puzz8_lab4 import *
+
+class Node:
+  def __init__(self, state = None, path = [], depth = 0, evalue = None):
+    self.thestate = state
+    self.thepath = path
+    self.thedepth = depth
+    self.theeval = evalue
+
+  def __eq__(self, other):
+    return self.thestate == other.thestate   # equality by state only!
+
+
+# parameterize the goal, successor and evaluation functions;
+
+def BEST_FIRST_SEARCH_FCT(start,target,GOAL_FCT,SUCCESSOR_FCT,EVAL_FCT,\
+                    COMPACT_PRINT = None):
+
+  if COMPACT_PRINT == None:
+      def myprint (x): print x
+      COMPACT_PRINT = myprint
+
+  open = [Node(start,[start],0,EVAL_FCT(start,target))]
+  closed = []
+  steps = 0  ######### report these as effort!!!
+  while open != []:
+    nxt = open[0]
+    open = open[1:]
+    
+    nxtstate = nxt.thestate  
+    nxtpath = nxt.thepath     
+    nxteval = nxt.theeval     
+  
+    if GOAL_FCT(nxtstate,target): 
+      print "GOAL FOUND:"
+      #print "state: %s" % nxtstate
+      print "State:   ",
+      COMPACT_PRINT(nxtstate)
+      print "PathL:  %d" % len(nxtpath)
+      print "Steps:  %d\n" % steps
+      return (nxtstate, nxtpath)
+
+    if nxt in closed:
+      continue
+    closed.append(nxt)
+
+    succ = SUCCESSOR_FCT(nxtstate) # successors are states, NOT Nodes (yet)
+    random.shuffle(succ)
+
+    for x in succ:
+      xcost = EVAL_FCT(x,target)
+      newnode = Node(x, addpath(nxtpath,x),0,\
+                     EVAL_FCT(x,target)) # 0 depth bec don't care
+
+      #check whether newnode is already on open or closed with
+      #shorter path; if so, do not bother to put open;
+      keeper = True
+      for c in closed:
+        if newnode.thestate == c.thestate and\
+           len(newnode.thepath) >= len(c.thepath):
+          keeper = False
+          break
+
+        if not keeper:
+          continue
+      
+      for op in open:
+        if newnode.thestate == op.thestate and\
+           len(newnode.thepath) >= len(op.thepath):
+          keeper = False
+          break
+
+      if keeper:
+          open.append(newnode)
+
+      open.sort(lambda x,y: CostCmp(x,y))  # NOTICE SORTING
+      steps += 1
+  return None
+
+def ASTAR_SEARCH_FCT(start,target,GOAL_FCT,SUCCESSOR_FCT,EVAL_FCT,\
+                    COMPACT_PRINT = None):
+
+  if COMPACT_PRINT == None:
+      def myprint (x): print x
+      COMPACT_PRINT = myprint
+
+  open = [Node(start,[start],0,EVAL_FCT(start,target))]
+  closed = []
+  steps = 0
+  while open != []:
+    nxt = open[0]
+    open = open[1:]
+    
+    nxtstate = nxt.thestate  
+    nxtpath = nxt.thepath
+    nxteval = nxt.theeval
+    nxtdpth = nxt.thedepth
+    
+    if GOAL_FCT(nxtstate,target): 
+      print "GOAL FOUND:"
+      #print "state: %s" % nxtstate
+      print "State:    ",
+      COMPACT_PRINT(nxtstate)
+      print "PathL:  %d" % len(nxtpath)
+      print "Steps:  %d\n" % steps
+      return (nxtstate, nxtpath)
+
+    
+    if nxt in closed:
+      continue
+    closed.append(nxt)  
+    
+    succ = SUCCESSOR_FCT(nxtstate) 
+    random.shuffle(succ)
+    
+    for x in succ:
+        xcost = EVAL_FCT(x,target)  
+        newnode = Node(x,addpath(nxtpath,x),nxtdpth+1,xcost+nxtdpth+1)
+
+        #check whether newstate[0] (or, x) is already on open or closed with
+        #shorter path; if so, do not bother to put open;
+        keeper = True
+        for c in closed:
+          if newnode.thestate == c.thestate and\
+             len(newnode.thepath) >= len(c.thepath):
+            keeper = False
+            break
+        if not keeper:
+          continue
+        
+        for op in open:
+          if newnode.thestate == op.thestate and\
+             len(newnode.thepath) >= len(op.thepath):
+            keeper = False
+            break
+
+        if keeper:
+            open.append(newnode)
+
+    open.sort(lambda x,y: CostCmp(x,y))  # NOTICE SORTING
+    steps += 1
+  return None
+
+
+# utility function 
+def addpath(path,x):
+  newpath = path[:]  # NOTICE: a copy is made!
+  newpath.append(x)
+  return newpath
+
+ # x,y are lists that are compared by
+# magnitude of their third elements;
+# x of type Node 
+# CostCmp compare costs, to allow sortig of triples by cost
+def CostCmp (x,y):
+  if x.theeval < y.theeval:
+    return -1
+  elif x.theeval == y.theeval:
+    return 0
+  else:
+    return 1
+
+# map puzz8 representation of puzzle state to
+# Tiles1to8_Frame object; allows graphics simulation
+# of path
+
+def make_1to8_init_list(puzz):
+  initlst = []
+  for x in range(1,4):
+    for y in range(1,4):
+      if puzz[(x,y)] == 'B':
+        initlst.append(0)
+      else:
+        initlst.append(puzz[(x,y)])
+  return initlst
+
+
+if __name__ == '__main__':
+
+  random.seed()
+
+  print "\nBest-first search in 8-puzzle domains:"
+  show_puzz8(puzzE)
+  for i in range(5):
+    BESTFIRST_SOLN =\
+                   BEST_FIRST_SEARCH_FCT(puzzE, GOAL, puzz8_goal_fct,\
+                                         puzz8_successor_fct,\
+                                         puzz8_eval_fct, puzz8_compact)
+
+  print "\nA* search in 8-puzzle domains:"
+  show_puzz8(puzzE)
+  for i in range(5):
+    ASTAR_SOLN =\
+               ASTAR_SEARCH_FCT(puzzE, GOAL, puzz8_goal_fct,\
+                                puzz8_successor_fct,\
+                                puzz8_eval_fct, puzz8_compact)
+
+  path1 = BESTFIRST_SOLN[1]
+  path2 = ASTAR_SOLN[1]
+
+  topickle = open("bestfirst_path.pickle", "wb")
+  pickle.dump(path1, topickle)
+  topickle.close()
+  
+  topickle = open("astar_path.pickle", "wb")
+  pickle.dump(path2,topickle)
+  topickle.close()
+  
+  print "\nA path from BEST-FIRST SEARCH:\n"
+  for x in path1:
+    puzz8_compact(x)
+
+  print "\nA path from ASTAR SEARCH:\n"
+  for x in path2:
+    puzz8_compact(x)
+
+    
+
+ 
+
+
+  
---
+++ Lab Week 4/3/main.py
@@ -0,0 +1,70 @@
+from bestfirst_astar_search_lab4 import *
+
+# first define goal -- 0 litres in 3l jug, 2 litres in 4l jug
+# we represent state as a simple 2-element list
+GOAL = [0, 2]
+
+
+# checking for goal is quite obvious
+def goal_fct(jugs, goal):
+	if jugs == goal:
+		return True
+	else:
+		return False
+
+
+# score is a distance from goal in litres (in both directions)
+def eval_fct(jugs, goal):
+	return abs(jugs[0]-goal[0]) + abs(jugs[1]-goal[1])
+
+
+def successor_fct(jugs):
+	moves = []
+
+	left, right = jugs  # ammount in left and right jugs
+
+	# non-empty left jug means we can empty it
+	if left > 0:
+		moves.append([0, right])
+	# non-full left jug means we can fill it
+	if left < 3:
+		moves.append([3, right])
+		# or pour from non-empty right jug
+		if right > 0:
+			moves.append([min(3, left + right), max(0, left + right - 3)])
+
+	# same as for left jug
+	if right < 4:
+		moves.append([left, 4])
+		if left > 0:
+			moves.append([min(3, left + right), max(0, left + right - 4)])
+	if right > 0:
+		moves.append([left, 0])
+
+	return moves
+
+
+# again, printer is pretty simple
+def compact(position):
+	print "%d | %d" % (position[0], position[1])
+
+
+def main():
+	print "Best-first solution. "
+	# run
+	BESTFIRST_SOLN = BEST_FIRST_SEARCH_FCT([0, 0], GOAL, goal_fct,
+		successor_fct, eval_fct, compact)
+
+	# and print. it all fits in one line, so no need for loops or something
+	print BESTFIRST_SOLN
+
+	print "\nAstar solution."
+	# repeat for A-star
+	ASTAR_SOLN = ASTAR_SEARCH_FCT([0, 0], GOAL, goal_fct,
+		successor_fct, eval_fct, compact)
+
+	print BESTFIRST_SOLN
+
+
+if __name__ == '__main__':
+	main()
