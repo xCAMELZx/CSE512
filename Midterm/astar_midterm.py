4ba4ceaf0c2195aa2c50edc1987dc778b1b60e56
Yousef Jarrar <005845836@coyote.csusb.edu>
2019-02-18 15:58:22 -0800

    Push Midterm

--- Midterm/astar_midterm.py
+++ Midterm/astar_midterm.py
@@ -2,7 +2,7 @@
 
 # STUDENT COMPLETER: ______________________
 
-# 
+#
 # Nodes with parent; compute solution path rather than
 # containing it within node
 
@@ -11,163 +11,164 @@ import time
 import operator
 from puzz8_midterm import *
 
-class Node:
-  def __init__(self, state = None, par = None, depth = 0, evalue = None):
-    self.thestate = state
-    #self.thepath = path
-    self.theparent = par
-    self.thedepth = depth
-    self.theeval = evalue
 
-  def __eq__(self, other):
-    return self.thestate == other.thestate   # equality by state only!
+class Node:
+    def __init__(self, state=None, par=None, depth=0, evalue=None):
+        self.thestate = state
+        # self.thepath = path
+        self.theparent = par
+        self.thedepth = depth
+        self.theeval = evalue
+
+    def __eq__(self, other):
+        return self.thestate == other.thestate  # equality by state only!
 
 
 # parameterize the goal, successor and evaluation functions;
 
 
-def ASTAR_SEARCH_FCT(start,target,GOAL_FCT,SUCCESSOR_FCT,EVAL_FCT,\
-                     COMPACT_PRINT = None):
+def ASTAR_SEARCH_FCT(start, target, GOAL_FCT, SUCCESSOR_FCT, EVAL_FCT, \
+                     COMPACT_PRINT=None):
+    if COMPACT_PRINT == None:
+        def myprint(x): print x
 
-  if COMPACT_PRINT == None:
-      def myprint (x): print x
-      COMPACT_PRINT = myprint
+        COMPACT_PRINT = myprint
 
-  open = [Node(start,None,0,EVAL_FCT(start,target))]
-  closed = []
-  steps = 0
-  while open != []:
-    nxt = open[0]
-    open = open[1:]
-    
-    nxtstate = nxt.thestate  
-    nxtpar = nxt.theparent
-    nxteval = nxt.theeval
-    nxtdpth = nxt.thedepth
-    
-    if GOAL_FCT(nxtstate,target):
+    open = [Node(start, None, 0, EVAL_FCT(start, target))]
+    closed = []
+    steps = 0
+    while open != []:
+        nxt = open[0]
+        open = open[1:]
 
-      # MIDTERM TAKEHOME PROBLEM
-      # define extract_path ... see below
-      nxtpath = extract_path(nxt,closed)
-      
-      print "GOAL FOUND:"
-      print "State:    ",
-      print nxtstate
-      print "PathL:  %d" % len(nxtpath)
-      print "Steps:  %d\n" % steps
-      # return 3 items of info: the last state, solution path,
-      # and number of search steps (iterations);
-      return [nxtstate, nxtpath,steps]
+        nxtstate = nxt.thestate
+        nxtpar = nxt.theparent
+        nxteval = nxt.theeval
+        nxtdpth = nxt.thedepth
 
-    
-    if nxt in closed:
-      # MIDTERM provided
-      adjust_parents(nxt,closed)
-      continue
-    
-    closed.append(nxt)  
-    
-    succ = SUCCESSOR_FCT(nxtstate) 
-    random.shuffle(succ)
-    
-    for x in succ:
-        xcost = EVAL_FCT(x,target)  
-        # Node nxt is parent of node with state x
-        newnode = Node(x,nxt,nxtdpth+1,xcost+nxtdpth+1)
+        if GOAL_FCT(nxtstate, target):
+            # MIDTERM TAKEHOME PROBLEM
+            # define extract_path ... see below
+            nxtpath = extract_path(nxt, closed)
 
-        #check whether newstate[0] (or, x) is already on open or closed with
-        #shorter path; if so, do not bother to put open;
-        keeper = True
-        for c in closed:
-          if newnode.thestate == c.thestate and\
-             newnode.thedepth >= c.thedepth:
-            keeper = False
-            break
-        if not keeper:
-          continue
-        
-        for op in open:
-          if newnode.thestate == op.thestate and\
-             newnode.thedepth >= op.thedepth:
-            keeper = False
-            break
+            print "GOAL FOUND:"
+            print "State:    ",
+            print nxtstate
+            print "PathL:  %d" % len(nxtpath)
+            print "Steps:  %d\n" % steps
+            # return 3 items of info: the last state, solution path,
+            # and number of search steps (iterations);
+            return [nxtstate, nxtpath, steps]
 
-        if keeper:
-            open.append(newnode)
+        if nxt in closed:
+            # MIDTERM provided
+            adjust_parents(nxt, closed)
+            continue
 
-    #open.sort(lambda x,y: CostCmp(x,y))  # NOTICE SORTING
-    # below works for Python 2 and 3 ...
-    open_plus = [(x,x.theeval) for x in open]
-    open_plus.sort(key = operator.itemgetter(1))
-    open = [x for (x,_) in open_plus]
-    
-    steps += 1
-  return None
+        closed.append(nxt)
+
+        succ = SUCCESSOR_FCT(nxtstate)
+        random.shuffle(succ)
+
+        for x in succ:
+            xcost = EVAL_FCT(x, target)
+            # Node nxt is parent of node with state x
+            newnode = Node(x, nxt, nxtdpth + 1, xcost + nxtdpth + 1)
+
+            # check whether newstate[0] (or, x) is already on open or closed with
+            # shorter path; if so, do not bother to put open;
+            keeper = True
+            for c in closed:
+                if newnode.thestate == c.thestate and \
+                        newnode.thedepth >= c.thedepth:
+                    keeper = False
+                    break
+            if not keeper:
+                continue
+
+            for op in open:
+                if newnode.thestate == op.thestate and \
+                        newnode.thedepth >= op.thedepth:
+                    keeper = False
+                    break
+
+            if keeper:
+                open.append(newnode)
+
+        # open.sort(lambda x,y: CostCmp(x,y))  # NOTICE SORTING
+        # below works for Python 2 and 3 ...
+        open_plus = [(x, x.theeval) for x in open]
+        open_plus.sort(key=operator.itemgetter(1))
+        open = [x for (x, _) in open_plus]
+
+        steps += 1
+    return None
+
 
 # MIDTERM provided
 def adjust_parents(nd, nodes):
-  for x in nodes:
-    if nd == x:
-      # see whether x needs adjusting
-      if nd.thedepth < x.thedepth:
-        # adjust ...
-        #print "adjusting parents backwards ... "
-        x.theparent = nd.parent
-        x.thedepth = nd.thedepth
-        x.theeval = nd.theeval
-        return
-      else:
-        #print "no need to adjust parents"
-        return
-  return
+    for x in nodes:
+        if nd == x:
+            # see whether x needs adjusting
+            if nd.thedepth < x.thedepth:
+                # adjust ...
+                # print "adjusting parents backwards ... "
+                x.theparent = nd.parent
+                x.thedepth = nd.thedepth
+                x.theeval = nd.theeval
+                return
+            else:
+                # print "no need to adjust parents"
+                return
+    return
 
-#************************************************************
+
+# ************************************************************
 # MIDTERM -- TAKEHOME PROBLEM
 # returns a list of states from start to goal;
 
-def extract_path(nd,closed):
-  # ... replace with correct body
+def extract_path(nd, closed):
+    path = []
+    # set current node to goal node
+    current = nd
+    # continue until parent node is None (parent of start node)
+    while current:
+        # add state of current node to the path
+        path.append(current.thestate)
+        # go to the parent node
+        current = current.theparent
+    # reverse path from start to goal
+    path.reverse()
 
-  return None  # place holder; replace with non-None
+    return path  # return the path
 
-#************************************************************
+
+# ************************************************************
 
 if __name__ == '__main__':
 
-  random.seed()
+    random.seed()
 
-  print "\nA* search in 8-puzzle domain:"
-  
-  stats = []
-  for puzz in allpuzz:
-    show_puzz8(puzz)
+    print "\nA* search in 8-puzzle domain:"
 
-    # run for #tiles-out-of-place as eval fct;
-    (_,pathA,stepsA)= ASTAR_SEARCH_FCT(puzz, GOAL, puzz8_goal_fct,\
-                                          puzz8_successor_fct,\
-                                          puzz8_eval_fct_A)
+    stats = []
+    for puzz in allpuzz:
+        show_puzz8(puzz)
 
-    # run for sum-of-horiz-and-vert-displacements as eval fct;
-    (_,pathB,stepsB)= ASTAR_SEARCH_FCT(puzz, GOAL, puzz8_goal_fct,\
-                                          puzz8_successor_fct,\
-                                          puzz8_eval_fct_B)
+        # run for #tiles-out-of-place as eval fct;
+        (_, pathA, stepsA) = ASTAR_SEARCH_FCT(puzz, GOAL, puzz8_goal_fct, \
+                                              puzz8_successor_fct, \
+                                              puzz8_eval_fct_A)
 
-    stats.append(((len(pathA),stepsA),(len(pathB),stepsB)))
+        # run for sum-of-horiz-and-vert-displacements as eval fct;
+        (_, pathB, stepsB) = ASTAR_SEARCH_FCT(puzz, GOAL, puzz8_goal_fct, \
+                                              puzz8_successor_fct, \
+                                              puzz8_eval_fct_B)
 
-  print "\n"
-  print "#tiles-out-of-place(A) vs Sum-vert-horiz-displace(B):\n"
-  for ((pA,sA),(pB,sB)) in stats:
-    print "A: pathlen %d at %d steps     B: pathlen %d at %d steps" %\
-          (pA,sA,pB,sB)
+        stats.append(((len(pathA), stepsA), (len(pathB), stepsB)))
 
-
-
-
-    
-                 
-
-
-
-
-  
+    print "\n"
+    print "#tiles-out-of-place(A) vs Sum-vert-horiz-displace(B):\n"
+    for ((pA, sA), (pB, sB)) in stats:
+        print "A: pathlen %d at %d steps     B: pathlen %d at %d steps" % \
+              (pA, sA, pB, sB)
--- Midterm/puzz8_midterm.py
+++ Midterm/puzz8_midterm.py
@@ -12,55 +12,54 @@
 import random
 import copy
 
+GOAL = {(1, 1): 1, (1, 2): 2, (1, 3): 3, \
+        (2, 1): 8, (2, 2): 'B', (2, 3): 4, \
+        (3, 1): 7, (3, 2): 6, (3, 3): 5}
 
-GOAL = {(1,1):1, (1,2):2, (1,3):3,\
-        (2,1):8, (2,2):'B', (2,3):4,\
-        (3,1):7, (3,2):6, (3,3):5}
+easy1 = {(1, 1): 1, (1, 2): 'B', (1, 3): 3, \
+         (2, 1): 8, (2, 2): 2, (2, 3): 4, \
+         (3, 1): 7, (3, 2): 6, (3, 3): 5}
 
-easy1 = {(1,1):1, (1,2):'B', (1,3):3,\
-        (2,1):8, (2,2):2, (2,3):4,\
-        (3,1):7, (3,2):6, (3,3):5} 
+easy2 = {(1, 1): 1, (1, 2): 2, (1, 3): 3, \
+         (2, 1): 7, (2, 2): 8, (2, 3): 4, \
+         (3, 1): 6, (3, 2): 'B', (3, 3): 5}
 
-easy2 = {(1,1):1, (1,2):2, (1,3):3,\
-        (2,1):7, (2,2):8, (2,3):4,\
-        (3,1):6, (3,2):'B', (3,3):5}
+puzzA = {(1, 1): 1, (1, 2): 'B', (1, 3): 3, \
+         (2, 1): 7, (2, 2): 2, (2, 3): 4, \
+         (3, 1): 6, (3, 2): 8, (3, 3): 5}
 
-puzzA = {(1,1):1, (1,2):'B', (1,3):3,\
-        (2,1):7, (2,2):2, (2,3):4,\
-        (3,1):6, (3,2):8, (3,3):5}
+puzzB = {(1, 1): 1, (1, 2): 3, (1, 3): 'B', \
+         (2, 1): 7, (2, 2): 2, (2, 3): 4, \
+         (3, 1): 6, (3, 2): 8, (3, 3): 5}
 
-puzzB = {(1,1):1, (1,2):3, (1,3):'B',\
-        (2,1):7, (2,2):2, (2,3):4,\
-        (3,1):6, (3,2):8, (3,3):5}
+puzzC = {(1, 1): 1, (1, 2): 3, (1, 3): 4, \
+         (2, 1): 7, (2, 2): 2, (2, 3): 5, \
+         (3, 1): 6, (3, 2): 8, (3, 3): 'B'}
 
+puzzD = {(1, 1): 1, (1, 2): 3, (1, 3): 4, \
+         (2, 1): 7, (2, 2): 2, (2, 3): 5, \
+         (3, 1): 'B', (3, 2): 6, (3, 3): 8}
 
-puzzC = {(1,1):1, (1,2):3, (1,3):4,\
-        (2,1):7, (2,2):2, (2,3):5,\
-        (3,1):6, (3,2):8, (3,3):'B'}
+puzzE = {(1, 1): 'B', (1, 2): 3, (1, 3): 4, \
+         (2, 1): 1, (2, 2): 2, (2, 3): 5, \
+         (3, 1): 7, (3, 2): 6, (3, 3): 8}
 
-puzzD = {(1,1):1, (1,2):3, (1,3):4,\
-        (2,1):7, (2,2):2, (2,3):5,\
-        (3,1):'B', (3,2):6, (3,3):8}
+challenge = {(1, 1): 8, (1, 2): 3, (1, 3): 5, \
+             (2, 1): 7, (2, 2): 1, (2, 3): 2, \
+             (3, 1): 6, (3, 2): 'B', (3, 3): 4}
 
-puzzE = {(1,1):'B', (1,2):3, (1,3):4,\
-        (2,1):1, (2,2):2, (2,3):5,\
-        (3,1):7, (3,2):6, (3,3):8}
-
-challenge = {(1,1):8, (1,2):3, (1,3):5,\
-             (2,1):7, (2,2):1, (2,3):2,\
-             (3,1):6, (3,2):'B', (3,3):4}
-
-allpuzz = [easy1,easy2,puzzA,puzzB,puzzC,puzzD,puzzE,challenge]
+allpuzz = [easy1, easy2, puzzA, puzzB, puzzC, puzzD, puzzE, challenge]
 
 
 def puzz8_hash(pz):
     hval = 0
-    for (x,y) in pz.keys():
-        if pz[(x,y)] == 'B':
-            hval  += (x+y)
+    for (x, y) in pz.keys():
+        if pz[(x, y)] == 'B':
+            hval += (x + y)
         else:
-            hval += (x+y) * pz[(x,y)]
+            hval += (x + y) * pz[(x, y)]
     return hval % 23
+
 
 # WARNING: generated puzzles may be very difficulty
 # or impossible to solve ... for testing, it is
@@ -73,14 +72,14 @@ def random_puzz8(k):
         yesno = raw_input("Keeper? [y/n] ")
         if yesno == 'y' or yesno == 'Y':
             return pz
-        
-            
+
+
 def make_puzz8(k):
-    pz = {(1,1):1, (1,2):2, (1,3):3,\
-            (2,1):8, (2,2):'B', (2,3):4,\
-            (3,1):7, (3,2):6, (3,3):5}
+    pz = {(1, 1): 1, (1, 2): 2, (1, 3): 3, \
+          (2, 1): 8, (2, 2): 'B', (2, 3): 4, \
+          (3, 1): 7, (3, 2): 6, (3, 3): 5}
     for i in range(k):
-        r = random.randint(1,4)
+        r = random.randint(1, 4)
         if r == 1:
             nxtpz = move_blank_up(pz)
             if nxtpz != None:
@@ -99,66 +98,85 @@ def make_puzz8(k):
                 pz = nxtpz
     return pz
 
+
 # print the puzzle
 def show_puzz8(pz):
     if pz == None:
         return
     print "\n"
-    for i in range(1,4):
-        for j in range(1,4):
-            print "%s " % pz[(i,j)],
+    for i in range(1, 4):
+        for j in range(1, 4):
+            print "%s " % pz[(i, j)],
         print "\n"
     return
 
+
 # compare corresponding tiles pz with the goal (global GOAL above)
 
-def puzz8_goal_fct(pz,goal):
+def puzz8_goal_fct(pz, goal):
     for k in goal.keys():
         if not goal[k] == pz[k]:
             return False
     return True
 
+
 # number of tiles out of place in pz relative to GOAL;
 #
-def puzz8_eval_fct_A(pz,goal):  #= None):
+def puzz8_eval_fct_A(pz, goal):  # = None):
     score = 0
     for k in goal.keys():
         if goal[k] != 'B' and not goal[k] == pz[k]:
             score += 1
     return score
 
-#*************************************************************
+
+# *************************************************************
 # MIDTERM -- TAKEHOME PROBLEM
 # sum of horizontal and vertical displacements for all tiles
 
-def puzz8_eval_fct_B(pz,goal):
-    # ... replace with correct body
+def puzz8_eval_fct_B(pz, goal):
 
-    return None  # place holder; replace witn non-None
+    # set sum to zero
+    sum = 0
+    # for each record in goal find key and value
+    for goal_key, goal_value in goal.items():
 
-#**************************************************************
+        row = goal_key[0]  # row
+        col = goal_key[1]  # column
+
+        # for each record in current node
+        for key, value in pz.items():
+            # find the record with the same value
+            if value == goal_value:
+                # add horizontal and vertical displacements to the sum
+                sum += abs(row - key[0]) + abs(col - key[1])
+                break
+
+    return sum
+
+# **************************************************************
 
 # puzz is a dictionary; keys are (r,c), values are tiles;
 def find_loc(tile, puzz):
-    for (r,c) in puzz.keys():
-        if puzz[(r,c)] == tile:
-            return (r,c)
-    return (-1,-1)   # something wrong if this line reached ...
+    for (r, c) in puzz.keys():
+        if puzz[(r, c)] == tile:
+            return (r, c)
+    return (-1, -1)  # something wrong if this line reached ...
+
 
 # try all move functions and collect all non-None results into a list
 # of puzzle successor states;
 # ... replace 'pass'
 def puzz8_successor_fct(pz):
-    succs =[]
-    moves = [move_blank_up(pz),\
-             move_blank_down(pz),\
-             move_blank_left(pz),\
+    succs = []
+    moves = [move_blank_up(pz), \
+             move_blank_down(pz), \
+             move_blank_left(pz), \
              move_blank_right(pz)]
     for x in moves:
         if not x == None:
             succs.append(x)
     return succs
-
 
 
 # moving a blank up ... B at (i,j) moves to (i-1,j), tile at (i-1,j)
@@ -170,9 +188,9 @@ def move_blank_up(pz):
     newpz = copy.deepcopy(pz)
     keys = newpz.keys()
     blank = None
-    for (i,j) in keys:
-        if newpz[(i,j)] == 'B':
-            blank = (i,j)
+    for (i, j) in keys:
+        if newpz[(i, j)] == 'B':
+            blank = (i, j)
             break
     # need to test whether blank can still move up;
     # if not return None
@@ -180,86 +198,90 @@ def move_blank_up(pz):
     if blank[0] == 1:
         # blank cannot move up
         return None
-    
-    #tile = newpz[(i-1,j)]
-    #newpz[(i-1,j)] = 'B'
-    #newpz[(i,j)] = tile
+
+    # tile = newpz[(i-1,j)]
+    # newpz[(i-1,j)] = 'B'
+    # newpz[(i,j)] = tile
     # ... or ... ONLY IN PYTHON ...
-    
-    newpz[(i-1,j)],newpz[(i,j)] = newpz[(i,j)],newpz[(i-1,j)]
+
+    newpz[(i - 1, j)], newpz[(i, j)] = newpz[(i, j)], newpz[(i - 1, j)]
     return newpz
-        
+
+
 def move_blank_down(pz):
     newpz = copy.deepcopy(pz)
     keys = newpz.keys()
     blank = None
-    for (i,j) in keys:
-        if newpz[(i,j)] == 'B':
-            blank = (i,j)
+    for (i, j) in keys:
+        if newpz[(i, j)] == 'B':
+            blank = (i, j)
             break
     if blank[0] == 3:
         # blank cannot move down
         return None
-    
-    #tile = newpz[(i+1,j)]
-    #newpz[(i+1,j)] = 'B'
-    #newpz[(i,j)] = tile
+
+    # tile = newpz[(i+1,j)]
+    # newpz[(i+1,j)] = 'B'
+    # newpz[(i,j)] = tile
     # or ... ONLY IN PYTHON ...
 
-    newpz[(i+1,j)],newpz[(i,j)] = newpz[(i,j)],newpz[(i+1,j)]
+    newpz[(i + 1, j)], newpz[(i, j)] = newpz[(i, j)], newpz[(i + 1, j)]
     return newpz
+
 
 def move_blank_left(pz):
     newpz = copy.deepcopy(pz)
     keys = newpz.keys()
     blank = None
-    for (i,j) in keys:
-        if newpz[(i,j)] == 'B':
-            blank = (i,j)
+    for (i, j) in keys:
+        if newpz[(i, j)] == 'B':
+            blank = (i, j)
             break
     if blank[1] == 1:
         # blank cannot move left
         return None
-    
-    #tile = newpz[(i,j-1)]
-    #newpz[(i,j-1)] = 'B'
-    #newpz[(i,j)] = tile
 
-    newpz[(i,j-1)],newpz[(i,j)] = newpz[(i,j)],newpz[(i,j-1)]
+    # tile = newpz[(i,j-1)]
+    # newpz[(i,j-1)] = 'B'
+    # newpz[(i,j)] = tile
+
+    newpz[(i, j - 1)], newpz[(i, j)] = newpz[(i, j)], newpz[(i, j - 1)]
     return newpz
+
 
 def move_blank_right(pz):
     newpz = copy.deepcopy(pz)
     keys = newpz.keys()
     blank = None
-    for (i,j) in keys:
-        if newpz[(i,j)] == 'B':
-            blank = (i,j)
+    for (i, j) in keys:
+        if newpz[(i, j)] == 'B':
+            blank = (i, j)
             break
     if blank[1] == 3:
         # blank cannot move left
         return None
-    
-    #tile = newpz[(i,j+1)]
-    #newpz[(i,j+1)] = 'B'
-    #newpz[(i,j)] = tile
-    
-    newpz[(i,j+1)],newpz[(i,j)] = newpz[(i,j)],newpz[(i,j+1)]
+
+    # tile = newpz[(i,j+1)]
+    # newpz[(i,j+1)] = 'B'
+    # newpz[(i,j)] = tile
+
+    newpz[(i, j + 1)], newpz[(i, j)] = newpz[(i, j)], newpz[(i, j + 1)]
     return newpz
+
 
 # print content of dictionary structure
 # more compactly for easy to ready output
 def puzz8_compact(puzz):
     print "[",
-    for row in [1,2,3]:
+    for row in [1, 2, 3]:
         print "[",
-        for col in [1,2,3]:
-            print "%s " % puzz[(row,col)],
+        for col in [1, 2, 3]:
+            print "%s " % puzz[(row, col)],
         print "]",
     print "]"
     return
-            
-    
+
+
 
 
 
